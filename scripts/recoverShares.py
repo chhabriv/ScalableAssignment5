560a78ba2b9fad4db3e29a5874472e4c090c7e3d
chhabriv <viren.chhabria@gmail.com>
2018-11-02 18:53:10 +0000

    level5

--- scripts/recoverShares.py
+++ scripts/recoverShares.py
@@ -63,7 +63,7 @@ def pwds_shares_to_secret(kpwds,kinds,diffs):
 #    ball= json.load(f)
  
     
-jsonFile = open("json\level4.json","r")
+jsonFile = open("json\level5.json","r")
 jsonStr = jsonFile.read()
 ball = jsonpickle.decode(jsonStr)	
 jsonFile.close()
@@ -77,7 +77,7 @@ shares=[x.encode('utf-8') for x in shares]
 words=[]
 kinds=[]
 #def retrieve():
-file2 = open("potfiles\level4.potfile")
+file2 = open("potfiles\level5.potfile")
 broken=file2.readlines()    
 # Create dict from hashes to shares
 hashesToShares = {}
@@ -117,7 +117,7 @@ def decrypt(enc, password):
 levelsecret=pwds_shares_to_secret(words,kinds,shares)
 print(levelsecret)
 decrypted = decrypt(ciphertext, levelsecret.zfill(32).decode('hex'))
-csname="level5"+".json"
+csname="level6"+".json"
 path=os.path.join("json\\",csname)
 with open(path,"w") as tmpf:
     tmpf.write(decrypted+"\n")
@@ -125,20 +125,20 @@ tmpf.close()
 newball=jsonpickle.decode(bytes.decode(decrypted))
 new_ciphertext=newball["ciphertext"]
 #new_ciphertext=str([x.encode('utf-8') for x in new_ciphertext])
-csname="level5"+".ciphertext"
+csname="level6"+".ciphertext"
 path=os.path.join("ciphertext\\",csname)
 with open(path,"w") as tmpf:
     tmpf.write(new_ciphertext+"\n")
 tmpf.close()
 new_hashes=newball["hashes"]
-csname="level5"+".hashes"
+csname="level6"+".hashes"
 path=os.path.join("hashes\\",csname)
 with open(path,"w") as tmpf:
     for hash in new_hashes:
         tmpf.write(hash+"\n")
 tmpf.close()
 new_shares=newball["shares"]
-csname="level5"+".shares"
+csname="level6"+".shares"
 path=os.path.join("shares\\",csname)
 with open(path,"w") as tmpf:
     for share in new_shares:
